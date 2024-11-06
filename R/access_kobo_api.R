0d54dab3bf9be999a9ca7765ec80c7f26c4b8229
alex-stephenson <49374679+alex-stephenson@users.noreply.github.com>
2024-11-06 10:36:30 +0300

    new functions and update to get_kobo_data to change sep

--- R/access_kobo_api.R
+++ R/access_kobo_api.R
@@ -11,34 +11,32 @@ get_password <- function(un) {
 
 #' Kobo Server Query function
 #' A function that allows you to retrieve kobo data from the server by providing the asset ID
-#' @param asset_id What is the asset ID of the survey youre interested in? 
-#' @keywords asset
+#' @param asset_id What is the asset ID of the survey youre interested in?
 #' @export
 #' @examples #kobo_data <- get_kobo_data(asset_id = "a38DR4AhAH2rDqP6e5YHLm") # TEST SURVEY
-#' @get_kobo
 
 get_kobo_data <- function(asset_id){
-  
-  
+
+
   if (!require("keyring", quietly = TRUE)) install.packages("keyring")
   if (!require("robotoolbox")) remotes::install_gitlab("dickoa/robotoolbox")
   library(keyring)
   library(robotoolbox)
-  
-  
+
+
   un = readline(prompt = "Please input your Koob server username: ")
   pw = get_password(un)
   kobo_server_url <- "https://kobo.impact-initiatives.org/"
   kobo_token_value <- kobo_token(username = un, password = pw, url = sub("\\/$","", kobo_server_url) )
   kobo_setup(url = sub("\\/$","", kobo_server_url), token = kobo_token_value)
-  
+
   form_uid <- asset_id
 
   asset <- kobo_asset(form_uid)
-  tmp_kobolist <- kobo_data(asset)
-  
+  tmp_kobolist <- kobo_data(asset, select_multiple_sep = "/")
+
   tmp_kobolist
-  
+
 }
 
 ##kobo_data <- get_kobo_data(asset_id = "a38DR4AhAH2rDqP6e5YHLm") # TEST SURVEY
--- R/stratified_sampling.R
+++ R/stratified_sampling.R
@@ -1,15 +1,20 @@
-set.seed(1)
 
-
+#' Calculate stratified sample
+#' @description A function that allows you sample a population based on strata. It will output the _sampled_ population, meaning the returned data frame will be your sample output.
+#' @param data the dataframe you want to sample
+#' @param grp_by the group or strata
+#' @param conf_level the confidence level
+#' @param margin_of_error the margin of error
+#' @param prop the proportion, default = 0.5
+#' @export
+#' @examples output <- calc_strat_samples(data =  DSA_VIII_data, grp_by = Neighbourhood, conf_level = 0.95, margin_of_error = 0.05)
 calc_strat_samples <- function(data, grp_by, conf_level, margin_of_error, prop = 0.5) {
-  
-  library(dplyr)
-  
+
   calc_size <- function(pop) {
  #   sample_size <- ((1.96^2 * 0.5 * (1 - 0.5)) / (0.05^2)) / (1 + (((1.96^2 * 0.5 * (1 - 0.5)) / (0.05^2)) - 1) / pop)
-    
+
     z_score <- qnorm((1 + conf_level) / 2)
-    
+
 
     # Calculate sample size using the formula
     sample_size <- (z_score^2 * prop * (1 - prop)) / (margin_of_error^2)
@@ -17,34 +22,31 @@ calc_strat_samples <- function(data, grp_by, conf_level, margin_of_error, prop =
     sample_size <- ceiling(sample_size)
     return(sample_size)
   }
-  
+
   summarised_DSAs <- function(data, grp_by) {
     data_summarised <- data %>%
-      group_by({{grp_by}}) %>%
-      summarise(n = n())
-    
+      dplyr::group_by({{grp_by}}) %>%
+      dplyr::summarise(n = n())
+
     return(data_summarised)
   }
-  
+
   grp_output <- summarised_DSAs(data, grp_by = Neighbourhood)
   grp_output$sample <- sapply(grp_output$n, calc_size)
 
   data_nested <- data %>%
-    nest(.by = {{grp_by}}) %>%
-    left_join(grp_output, by =join_by({{grp_by}} == {{grp_by}}))
-  
-  
+    tidyr::nest(.by = {{grp_by}}) %>%
+    dplyr::left_join(grp_output, by =join_by({{grp_by}} == {{grp_by}}))
+
+
   for (i in 1:length(data_nested$data)) {
     data_nested$data[[i]] <- sample_n(data_nested$data[[i]], data_nested$sample[[i]])
   }
-  
+
   data_unnested <- data_nested %>%
-    unnest(., data) %>%
-    mutate(incl = TRUE)
-  
+    tidyr::unnest(., data) %>%
+    dplyr::mutate(incl = TRUE)
+
   return(data_unnested)
 }
-
-
-#output <- calc_strat_samples(data =  DSA_VIII_data, grp_by = Neighbourhood, conf_level = 0.95, margin_of_error = 0.05)
 
